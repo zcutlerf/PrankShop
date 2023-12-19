e2fe65739c139be77bcd2da201c7888048bf5bd5
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2023-12-19 08:27:27 -0500

    Initial Commit

---
+++ PrankShop/PrankShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrankShopApp.swift
+//  PrankShop
+//
+//  Created by Zoe Cutler on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PrankShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
